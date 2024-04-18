f52c8dfec6d6ce345e39038a999e2e6bd694244c
yeonuleee <162788998+yeonuleee@users.noreply.github.com>
2024-04-18 17:55:19 +0900

    Update prime_number.py

--- week 3/prime_number.py
+++ week 3/prime_number.py
@@ -1,8 +1,9 @@
-n=input("숫자를 입력해 주세요.")
+n = input("숫자를 입력해 주세요.")
 for i in range(2, int(n)): #1과 2도 포함하고 싶다면 어떻게 하나요?
     if int(n)% i != 0:
         continue
     else:
         print(n+"은/는 소수가 아닙니다.")
         break 
-else:print(n+"은/는 소수입니다.")
+else:
+    print(n+"은/는 소수입니다.")
